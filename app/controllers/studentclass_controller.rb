d76bf093de28ef195241083663f95b74659eb2f0
Naomi Dennis <njdennis94@gmail.com>
2018-04-01 23:45:41 +0000

    automatically backed up by learn

--- app/controllers/studentclass_controller.rb
+++ app/controllers/studentclass_controller.rb
@@ -1,3 +1,27 @@
 class StudentClass < ApplicationController
 
+  
+  def index 
+  end 
+
+  def new 
+  end 
+
+  def show
+  end 
+  
+  def create
+  end
+
+  def edit 
+  end 
+
+  def update
+  end  
+
+  private 
+
+    def accept_params 
+      StudentClass.
+    end 
 end 
